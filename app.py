13aff0de30a34438c94a2fb93bd1605ab01eedd0
Anubhav Jain <anubhav94@gmail.com>
2015-04-28 23:23:03 -0400

    Made edits to now allow users to change text

--- app.py
+++ app.py
@@ -1,47 +1,3 @@
-# import os
-# from datetime import datetime
-# from flask import Flask, render_template, jsonify, redirect, url_for, request, send_from_directory
-
-# app = Flask(__name__)
-# app.config.from_object(__name__)
-# app.config['UPLOAD_FOLDER'] = 'uploads'
-
-# ALLOWED_EXTENSIONS = ['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif']
-
-
-# @app.route('/', methods=['GET', 'POST'])
-# def upload_file():
-#     if request.method == 'POST':
-#         file = request.files['file']
-#         if file and allowed_file(file.filename):
-#             now = datetime.now()
-#             filename = os.path.join(app.config['UPLOAD_FOLDER'], "%s.%s" % (now.strftime("%Y-%m-%d-%H-%M-%S-%f"), file.filename.rsplit('.', 1)[1]))
-#             file.save(filename)
-#     	    return redirect(url_for('uploaded_file', filename=filename))
-#     return '''
-#     <!doctype html>
-#     <title>Upload new File</title>
-#     <h1>Upload new File</h1>
-#     <form action="" method="post" enctype="multipart/form-data">
-#       <p><input type="file" name="file" accept="video/*;capture=camera">
-#          <input type="submit" value="Upload">
-#     </form>
-#     '''
-# def allowed_file(filename):
-#     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
-# @app.route('/show/<filename>')
-# def uploaded_file(filename):
-#     filename = 'http://127.0.0.1:5000/uploads/' + filename
-#     return render_template('template.html', filename=filename)
-
-# @app.route('/uploads/<filename>')
-# def send_file(filename):
-#     return send_from_directory(UPLOAD_FOLDER, filename)
-
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=5000, debug=True)
-
 import os
 from flask import Flask, request, redirect, url_for, send_from_directory, render_template
 from werkzeug.utils import secure_filename
@@ -59,13 +15,14 @@ def allowed_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        print request
         file = request.files['file']
-        if file and allowed_file(file.filename):
+        word = request.form['word']
+	color = request.form['color']
+	if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(file_path)
-            modify_image(file_path)
+            modify_image(file_path, word, color)
             modified_path = filename.split('.')[0] + '_modified.jpg'
             return redirect(url_for('send_file', filename=modified_path))
     return render_template('index.html')
--- test.py
+++ test.py
@@ -1,15 +1,17 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
-def modify_image(filename):
+def modify_image(filename, word, color):
   print filename
   background = Image.open(filename)
   bgw, bgh = background.size
   if not (bgw < bgh):
     background = background.rotate(-90)
+  background = background.resize((960, 1280))
   bgw, bgh = background.size
-  foreground = Image.open("arcelik.png")
-  resizew, resizeh = 640, 180
-  resized = foreground.resize((resizew, resizeh))
-  background.paste(resized, (0, bgh - resizeh), resized)
+  frame = Image.open(color + "Frame.png")
+  frame.paste(background, (74, 138))
+  draw = ImageDraw.Draw(frame)
+  font = ImageFont.truetype("BebasNeueRegular.ttf", 225)
+  draw.text((325, 1490),word.upper(),(255,255,255),font=font)
   removed_ending = filename.split('.')[0]
-  background.save(removed_ending + '_modified.jpg')
+  frame.save(removed_ending + '_modified.jpg')
